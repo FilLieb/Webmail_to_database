e767c8239a19e2223e4ce03867fc2ab738280d9d
FilLieb <117804244+FilLieb@users.noreply.github.com>
2024-09-03 21:40:21 +0200

    scribble created all variables and append to sheet

--- scribble.py
+++ scribble.py
@@ -1,64 +1,42 @@
 import openpyxl
-import os
 
-
-# Open existing workbook and select the active worksheet
-workbook = openpyxl.load_workbook('ColumnLogbook.xlsx')
-sheet = workbook.active
-
-
+# Body of the email as a string
 Body = "Name Patricia Brown Date 2024-06-28 Column used SEC-17 runs 6 End pressure 0.5 Flow rate 1 Column cleaned ? no solution equilibrated Ethanol solution Errors/Comments Great it was a pleasure working with you"
 
-words = Body.split()
-
-def nextword(target, source):
-    for i, w in enumerate(source):
-        if w == target:
-            return source[i+1]
-
-def find_between(Body, first, last ):
+#functions to receive information
+def find_between(text, first, last ):
     try:
-        start = Body.index( first ) + len( first )
-        end = Body.index( last, start )
-        return Body[start:end]
+        start = text.index( first ) + len( first )
+        end = text.index( last, start )
+        return text[start:end]
     except ValueError:
         return ""
 
-def substring_after(s, delim):
-    return s.partition(delim)[2]
+def substring_after(text, after):
+    return text.partition(after)[2]
 
-
+# create variables with info
 name = find_between(Body, "Name ", "Date")
-
-date = nextword("Date", words)
-
-column = nextword("used", words)
-
-runs = nextword("runs", words)
-
-pressure = nextword("pressure", words)
-
-flow = nextword("rate", words)
-
-clean = nextword("?", words)
-
+date = find_between(Body, "Date ", "Column")
+column = find_between(Body, "used ", "runs")
+runs = find_between(Body, "runs ", "End")
+pressure = find_between(Body, "pressure ", "Flow")
+flow = find_between(Body, "rate ", "Column")
+clean = find_between(Body, "? ", "solution")
 solution = find_between(Body, "equilibrated ", "Errors/Comments")
-
 comments = substring_after(Body, "Errors/Comments ")
 
-print(name)
-print(date)
-print(column)
-print(runs)
-print(pressure)
-print(flow)
-print(clean)
-print(solution)
-print(comments)
+# add variable to data frame
+new_data = [[name, date, column, runs, pressure, flow, clean, solution, comments]]
 
 
+# Open existing workbook and select the active worksheet
+workbook = openpyxl.load_workbook('ColumnLogbook_QR.xlsx')
+sheet = workbook.active
 
-#sheet.append(['Subject', 'From', 'Date', 'Body'])
+# Append new data
+for row in new_data:
+    sheet.append(row)
 
 # Save the workbook
-#workbook.save('ColumnLogbook.xlsx')
+workbook.save('ColumnLogbook_QR.xlsx')
